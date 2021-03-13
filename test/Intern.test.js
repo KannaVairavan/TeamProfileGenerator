59b67b5e3742ac664968d8954b787f4a85867855
Kannathal Vairavan <kannamanickam@gmail.com>
2021-03-12 19:56:07 -0600

    Modified Intern.testjs with test cases

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -2,27 +2,27 @@ const Engineer = require("../lib/Intern.js");
 describe("Intern", () => {
     
 
-      // 'set' GitHub account"
-      it("Can set GitHUb account via constructor", () => {
-        const gitTestValue = "GitHubUser";
-        const eng = new Engineer("Joe", 1, "test@test.com", gitTestValue);
+      // 'set' School Name"
+      it("Can find school name via constructor", () => {
+        const testSchool = "schoolName";
+        const int = new Intern("Joe", 1, "test@test.com", testSchool);
         // We expect 'result' to equal to test value' function is working correctly.
-        expect(eng.github).toBe(gitTestValue);
+        expect(int.school).toBe(testSchool);
              
       });
 
     //   test title
-    it("getRole function should return Engineer", () =>{
-        const testValue="Engineer";
-        const eng= new Engineer("Joe", 1, "test@test.com", testValue);
-        expect(eng.getRole()).toBe(testValue);
+    it("getRole function should return Intern", () =>{
+        const testValue="Intern";
+        const int= new Intern("Joe", 1, "test@test.com", testValue);
+        expect(int.getRole()).toBe(testValue);
     });
     
-    // test github function
-    it("getgithub function shoult return GitHub user name",()=>{
-        const testGitHubUser="UserName"
-        const eng=new Engineer("Mike",2,"mike@test.com",testGitHubUser);
-        expect(eng.getgithub()).toBe(testGitHubUser);
+    // test school function
+    it("getSchool function shoult return  school name",()=>{
+        const testSchoolName="UT Austin"
+        const int=new Intern("Mike",2,"mike@test.com",testSchoolName);
+        expect(int.getSchool()).toBe(testSchoolName);
     });
 
 
